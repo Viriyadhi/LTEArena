efbd8dbc74e7efa307024d6ac82f608b20e71f06
Sarah Firyal <sarah5firyal@gmail.com>
2021-11-13 12:39:22 +0700

    feat: [SF] fragments

--- app/src/main/java/com/example/ltearena/activity/LoginActivity.java
+++ app/src/main/java/com/example/ltearena/activity/LoginActivity.java
@@ -73,8 +73,7 @@ public class LoginActivity extends AppCompatActivity {
                             if (task.isSuccessful()) {
                                 // Sign in success, update UI with the signed-in user's information
                                 Log.d("TAG", "signInWithEmail:success");
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                Intent intent = new Intent(getApplicationContext(), HomeScreen.class);
+                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                                 startActivity(intent);
                                 finish();
                             } else {
